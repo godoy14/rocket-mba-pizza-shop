5bea511f938f262aab7752fa372d414754f6f2eb
godoy14 <joao.sgodoy@hotmail.com>
2025-03-04 10:47:41 -0300

    Adicionado Filtros e Paginação

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
+
+
+export function OrderTableRow() {
+    return (
+
+        <TableRow>
+            <TableCell>
+                <Button variant="outline" size="sm">
+                    <Search className="h-3 w-3" />
+                    <span className="sr-only">Detalhes do pedido</span>
+                </Button>
+            </TableCell>
+            <TableCell className="font-mono text-xs font-medium">
+                123456789
+            </TableCell>
+            <TableCell className="text-muted-foreground">
+                há 15 minutos
+            </TableCell>
+            <TableCell>
+                <div className="flex items-center gap-2">
+                    <span className="h-2 w-2 rounded-full bg-slate-400" />
+                    <span className="font-medium text-muted-foreground">
+                        Pendente
+                    </span>
+                </div>
+            </TableCell>
+            <TableCell className="font-medium">
+                João Godoy
+            </TableCell>
+            <TableCell className="font-medium">
+                R$ 150,50
+            </TableCell>
+            <TableCell>
+                <Button variant="outline" size="sm">
+                    <ArrowRight className="mr-2 h-3 w-3" />
+                    Aprovar
+                </Button>
+            </TableCell>
+            <TableCell>
+                <Button variant="ghost" size="sm">
+                    <X className="mr-2 h-3 w-3" />
+                    Aprovar
+                </Button>
+            </TableCell>
+        </TableRow>
+
+    )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,78 +1,38 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
+
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "@/components/pagination";
 
 export function Orders() {
     return (
         <>
             <div className="flex flex-col gap-4">
                 <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-            </div>
+                <div className="space-y-2.5">
+                    <OrderTableFilters />
+                    <div className="border rounded-md">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[64px]"></TableHead>
+                                    <TableHead className="w-[140px]">Identificador</TableHead>
+                                    <TableHead className="w-[180px]">Realizado há</TableHead>
+                                    <TableHead className="w-[140px]">Status</TableHead>
+                                    <TableHead>Cliente</TableHead>
+                                    <TableHead className="w-[140px]">Total do cliente</TableHead>
+                                    <TableHead className="w-[164px]"></TableHead>
+                                    <TableHead className="w-[132px]"></TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <OrderTableRow />
+                            </TableBody>
+                        </Table>
+                    </div>
 
-            <div>
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-                </form>
-
-                <div className="border rounded-md">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[64px]"></TableHead>
-                                <TableHead className="w-[140px]">Identificador</TableHead>
-                                <TableHead className="w-[180px]">Realizado há</TableHead>
-                                <TableHead className="w-[140px]">Status</TableHead>
-                                <TableHead>Cliente</TableHead>
-                                <TableHead className="w-[140px]">Total do cliente</TableHead>
-                                <TableHead className="w-[164px]"></TableHead>
-                                <TableHead className="w-[132px]"></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell>
-                                    <Button variant="outline" size="sm">
-                                        <Search className="h-3 w-3" />
-                                        <span className="sr-only">Detalhes do pedido</span>
-                                    </Button>
-                                </TableCell>
-                                <TableCell className="font-mono text-xs font-medium">
-                                    123456789
-                                </TableCell>
-                                <TableCell className="text-muted-foreground">
-                                    há 15 minutos
-                                </TableCell>
-                                <TableCell>
-                                    <div className="flex items-center gap-2">
-                                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                                        <span className="font-medium text-muted-foreground">
-                                            Pendente
-                                        </span>
-                                    </div>
-                                </TableCell>
-                                <TableCell className="font-medium">
-                                    João Godoy
-                                </TableCell>
-                                <TableCell className="font-medium">
-                                    R$ 150,50
-                                </TableCell>
-                                <TableCell>
-                                    <Button variant="outline" size="sm">
-                                        <ArrowRight className="mr-2 h-3 w-3" />
-                                        Aprovar
-                                    </Button>
-                                </TableCell>
-                                <TableCell>
-                                    <Button variant="ghost" size="sm">
-                                        <X className="mr-2 h-3 w-3" />
-                                        Aprovar
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
+                    <Pagination pageIndex={0} totalCount={100} perPage={10} />
                 </div>
             </div>
         </>
